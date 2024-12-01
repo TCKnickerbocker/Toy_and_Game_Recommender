9db895b577af7c12987213c5516078f178791134
Owen Ratgen <ratge006@umn.edu>
2024-12-01 13:41:16 -0600

    Same

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,6 +1,7 @@
 // Top level component, everything runs through this
 import { Outlet } from "react-router-dom";
 
+// ? FUTURE: Find a useful MUI component to wrap a page in
 export default function App() {
   return (
     <div className="body">

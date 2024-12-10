0b7177065351a1ad1b2c4201982a2c56773b1865
TCKnickerbocker <74137392+TCKnickerbocker@users.noreply.github.com>
2024-12-09 20:47:13 -0600

    Models 2, 3, and 4 working. Added dockerfiles for models 2, 3, & 4 as well

--- src/models/generate_new_products/call_generate_model.py
+++ src/models/generate_new_products/call_generate_model.py
@@ -39,4 +39,4 @@ def generate_products():
     return jsonify(generated_products), 200
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5004)
+    app.run(debug=True, host="0.0.0.0", port=5007)
---
+++ src/models/model_2/model_2_alg.py
@@ -0,0 +1,20 @@
+
+def recommend_products(most_similar_products, num_recs_to_give):
+    # Calculate weights based on ratings
+    print(len(most_similar_products))
+    total_ratings = sum([info[0] for info in most_similar_products.values()])
+    weights = {pid: info[0] / total_ratings for pid, info in most_similar_products.items()}
+    
+    recommendations = []
+    for pid, weight in weights.items():
+        num_recs = round((weight * num_recs_to_give))
+        if num_recs == 0:
+            num_recs = 1
+        print(most_similar_products[pid][2][:num_recs])
+        recommendations.extend(most_similar_products[pid][2][:num_recs])
+    
+    # Ensure unique recommendations and limit to num_recs_to_give
+    recommendations = list(dict.fromkeys(recommendations))[:num_recs_to_give]
+    print(num_recs_to_give)
+    print(len(recommendations))
+    return recommendations
